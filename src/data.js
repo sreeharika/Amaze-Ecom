e2c45047cda08b4a77614e06c8ad0f6ac6eaeb7a
Sreeharika <sreeharikayelle@gmail.com>
2020-09-16 13:44:51 +0530

    render product list

---
+++ src/data.js
@@ -0,0 +1,79 @@
+export default {
+    products: [
+        {
+        name: 'Slim shrit',
+        category: 'Shrits',
+        image: '/images/d1.jpg',
+        price: 599,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10
+        },
+        {
+            name: 'Slim shrit',
+            category: 'Shrits',
+            image: '/images/d2.jpg',
+            price: 799,
+            brand: 'Polo',
+            rating: 4,
+            numReviews: 100
+        },
+        {
+            name: 'Slim shrit',
+            category: 'Shrits',
+            image: '/images/d3.jpg',
+            price: 399,
+            brand: 'Max',
+            rating: 3.5,
+            numReviews: 20
+        },
+        {
+            name: 'T-Shrit',
+            category: 'T-Shrits',
+            image: '/images/t1.jpg',
+            price: 999,
+            brand: 'Max',
+            rating: 4.5,
+            numReviews: 15
+        },
+        {
+            name: 'T-Shrit',
+            category: 'T-Shrits',
+            image: '/images/t2.jpg',
+            price: 385,
+            brand: 'USpolo',
+            rating: 4,
+            numReviews: 105
+        },
+        {
+            name: 'T-Shrit',
+            category: 'T-Shrits',
+            image: '/images/t3.jpg',
+            price: 599,
+            brand: 'GAP',
+            rating: 4.5,
+            numReviews: 5
+        },
+        {
+            name: 'Pants',
+            category: 'Pants',
+            image: '/images/p1.jpg',
+            price: 1099,
+            brand: 'Levis',
+            rating: 3.5,
+            numReviews: 10
+        },
+        {
+            name: 'Pants',
+            category: 'Pants',
+            image: '/images/p2.jpg',
+            price: 1299,
+            brand: 'Spykar',
+            rating: 4,
+            numReviews: 0
+        },
+               
+
+    ]
+}
+
